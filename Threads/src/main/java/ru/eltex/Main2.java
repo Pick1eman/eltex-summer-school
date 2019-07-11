aedd2050cc325f62c339527cb75490b03c896034
Stalker2973 <omelete@mail.ru>
2019-07-11 15:41:55 +0700

    Pool threads

--- Threads/src/main/java/ru/eltex/Main2.java
+++ Threads/src/main/java/ru/eltex/Main2.java
@@ -1,16 +1,28 @@
 package ru.eltex;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 class Main2
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
+        //ExecutorService exec = Executors.newCachedThreadPool();
+        ExecutorService exec = Executors.newFixedThreadPool(10);
         Runnable r = new MyThread();
-        for (int i = 0; i < 10; i++)
-        {
-            Thread th = new Thread(r);
-            th.start();
 
+        long startTimer = System.nanoTime();
+
+        for (int i = 0; i < 10000; i++)
+        {
+            exec.submit(r);
         }
+        exec.shutdown();
+        exec.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        long stopTimer = System.nanoTime();
+        long elapsed = stopTimer - startTimer;
+
+        System.out.println("Timer = " + elapsed*Math.pow(10,-9));
+
     }
 
 }
