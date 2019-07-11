be6181be446748589855e70d2ae5fc65ffae8bd6
Stalker2973 <omelete@mail.ru>
2019-07-11 16:18:05 +0700

    Reflection

--- Accounts/src/main/java/ru/eltex/Main.java
+++ Accounts/src/main/java/ru/eltex/Main.java
@@ -1,15 +1,23 @@
 package ru.eltex;
+import java.lang.reflect.Field;
 import java.util.Scanner;
 
 class Main{
     public static void main(String args[])
     {
-        Manager a = new Manager();
+        /*Manager a = new Manager();
         Developer b = new Developer();
         a.readFromFileManager("/home/user/Рабочий стол/eltex-summer-school/Accounts/src/main/resources/Managers.csv");
         a.printListManager();
         b.readFromFileDeveloper("/home/user/Рабочий стол/eltex-summer-school/Accounts/src/main/resources/Developers.csv");
         b.printListDeveloper();
+        */
+        Class<User> userClass = User.class;
+        Field[] userFiled = userClass.getDeclaredFields();
+        for (Field fie: userFiled)
+        {
+            System.out.println(fie.getName());
+        }
     }
 
 }
