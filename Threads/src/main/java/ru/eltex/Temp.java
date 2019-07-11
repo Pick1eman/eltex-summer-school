baa6cb67f0f1ad1a151cbdce3a77c504848f4904
Stalker2973 <omelete@mail.ru>
2019-07-11 14:11:00 +0700

    Threads mutex

--- Threads/src/main/java/ru/eltex/Temp.java
+++ Threads/src/main/java/ru/eltex/Temp.java
@@ -1,17 +1,23 @@
 package ru.eltex;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 class Temp
 {
-    static private volatile int a = 0;
+    static private int a = 0;
+    static ReentrantLock lock = new ReentrantLock();
     Temp()
     {
     }
 
     static public void counter()
     {
+        lock.lock();
         a = a + 1;
         System.out.println("a = " + a);
+        lock.unlock();
     }
+
 
 
 }
