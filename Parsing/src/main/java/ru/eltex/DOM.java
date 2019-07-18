df17fd94732ec961b069ae27023ee941e60f878f
Stalker2973 <omelete@mail.ru>
2019-07-18 13:44:00 +0700

    First parsing

---
+++ Parsing/src/main/java/ru/eltex/DOM.java
@@ -0,0 +1,38 @@
+package ru.eltex;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+class DOM
+{
+    private static ArrayList<String> msgs = new ArrayList<>();
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File("resources/temp.xml"));
+        NodeList msgElements = document.getDocumentElement().getElementsByTagName("msg");
+        for (int i = 0; i < msgElements.getLength(); i++)
+        {
+            Node msg = msgElements.item(i);
+            NamedNodeMap attributes = msg.getAttributes();
+            //msgs.add(new msg(attributes.getNamedItem("to").getNodeValue(),attributes.getNamedItem("from").getNodeValue(),attributes.getNamedItem("title").getNodeValue()));
+            System.out.println(attributes.getNamedItem("to").getNodeValue() + " " + attributes.getNamedItem("from").getNodeValue() + " " + attributes.getNamedItem("title").getNodeValue());
+        }
+        /*for (String msg : msgs)
+        {
+            System.out.println(msg);
+        }*/
+    }
+}
