abd544607bfb31fd72df1918232bce65afb4a531
Stalker2973 <omelete@mail.ru>
2019-07-23 12:04:38 +0700

    Test transaction

--- MySQL/src/main/java/ru/eltex/Main.java
+++ MySQL/src/main/java/ru/eltex/Main.java
@@ -3,15 +3,14 @@ import java.sql.*;
 import java.util.Scanner;
 
 class Main{
-    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/users";
+    //public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/users";
     public static void main(String args[])
     {
-        Developer developer = new Developer();
         Manager manager = new Manager();
-        try{
+        manager.tempAdd();
+        /*try{
             Connection connection = DriverManager.getConnection(DB_URL,"root","2973");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from developers");
             developer.readFromFileDeveloper("/home/user/Рабочий стол/eltex-summer-school/MySQL/resources/Developers.csv");
             manager.readFromFileManager("/home/user/Рабочий стол/eltex-summer-school/MySQL/resources/Managers.csv");
             connection.close();
@@ -19,6 +18,6 @@ class Main{
         {
             System.out.println(error.getMessage());
         }
-
+*/
     }
 }
--- MySQL/src/main/java/ru/eltex/Manager.java
+++ MySQL/src/main/java/ru/eltex/Manager.java
@@ -1,9 +1,13 @@
 package ru.eltex;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.lang.Math.pow;
 
 
 class Manager extends User
@@ -48,7 +52,7 @@ class Manager extends User
                     String[] str2 = str.split(" \\|\\| ");
                     super.fromCSV(str2[0]);
                     manager.close();
-                    //String[] temp = str2[1].split("; ");
+                    String[] temp = str2[1].split("; ");
                     String[] str3 = str2[0].split("; ");
                     String[] tmp2 = str2[1].split(": ");
                     /*for (String tmp : temp) {
@@ -76,5 +80,75 @@ class Manager extends User
     public void printListManager()
     {
         System.out.println("ID = " + super.getId() + "; FIO = " + super.getFio() + "; E-mail = " + super.getEmail() + "; Phone = " + super.getPhone() + " || " + this.product.toString());
+    }
+
+    public void readFromMySQL()
+    {
+        try {
+            String DB_URL = "jdbc:mysql://127.0.0.1:3306/users";
+            String pathFile = "/user/home/Рабочий стол/eltex-summer-school/MySQL/resources";
+            Connection connection = DriverManager.getConnection(DB_URL,"root","2973");
+            try {
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("select * from managers");
+                FileWriter fileWriter = new FileWriter(pathFile);
+                while (resultSet.next())
+                {
+                    Integer id = resultSet.getInt("id");
+                    String fio = resultSet.getString("fio");
+                    String email = resultSet.getString("email");
+                    String phone = resultSet.getString("phone");
+                    String title = resultSet.getString("title");
+                    String price = resultSet.getString("price");
+                    System.out.println(id + fio + email + phone + title + price);
+                    fileWriter.write(id + "; " + fio + "; " + email + "; " + phone + "; " + title + "; " + price + "\n");
+                }
+
+            } catch (SQLException error) {
+                System.out.println(error.getMessage());
+            } finally {
+                connection.close();
+            }
+        } catch (IOException | SQLException error) {
+            System.out.println(error.getMessage());
+        }
+    }
+
+    public void tempAdd()
+    {
+        try
+        {
+            String DB_URL = "jdbc:mysql://127.0.0.1:3306/users";
+            Connection connection = DriverManager.getConnection(DB_URL,"root","2973");
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("delete from managers where id >= 0");
+            connection.setAutoCommit(false);
+
+            long startTimer = System.nanoTime();
+            for (int i = 0; i < 1000; i++)
+            {
+                statement.executeUpdate("INSERT INTO managers VALUE (" + i + ",'is741s" + i + "','temp2','asdsa','asdasf',3000);");
+            }
+
+            long stopTimer = System.nanoTime();
+            long elapsed = stopTimer - startTimer;
+            System.out.println("Timer with tran = " + elapsed/pow(10,9) + " seconds");
+            connection.commit();
+
+            connection.setAutoCommit(true);
+            statement.executeUpdate("delete from managers where id >= 0");
+            startTimer = System.nanoTime();
+            for (int i = 0; i < 1000; i++)
+            {
+                statement.executeUpdate("INSERT INTO managers VALUE (" + i + ",'is741s" + i + "','temp2','asdsa','asdasf',3000);");
+            }
+            stopTimer = System.nanoTime();
+            elapsed = stopTimer - startTimer;
+            System.out.println("Timer w/o tran = " + elapsed/pow(10,9) + " seconds");
+            connection.close();
+        } catch (SQLException error)
+        {
+            System.out.println(error.getErrorCode());
+        }
     }
 }
