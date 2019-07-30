f6edcaf3c16ddb97f461031b34fcfac95f346b64
Stalker2973 <omelete@mail.ru>
2019-07-30 14:07:51 +0700

    MongoDB

--- MongoDB(simple)/src/main/java/ru/eltex/Main.java
+++ MongoDB(simple)/src/main/java/ru/eltex/Main.java
@@ -1,25 +1,39 @@
 package ru.eltex;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-import java.net.UnknownHostException;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Main
 {
-    public static void main(String[]args) throws UnknownHostException
+    public static void main(String[]args) throws  IOException
     {
         Properties prop = new Properties();
-        MongoClient mongoClient = MongoClients.create(prop.getProperty("host"));
-        MongoDatabase db = mongoClient.getDatabase(prop.getProperty("dbname"));
-        MongoCollection<Document> table = db.getCollection(prop.getProperty("table"));
+        FileInputStream fis = new FileInputStream("src/main/resources/db.properties");
+        prop.load(fis);
+        MongoClient mongoClient = MongoClients.create(prop.getProperty("db.host"));
+        MongoDatabase db = mongoClient.getDatabase(prop.getProperty("db.name"));
+        MongoCollection<Document> table = db.getCollection(prop.getProperty("db.table"));
+        table.deleteMany(new BasicDBObject());
+
 
         Document document = new Document("name","Vyanceslav").append("phone","1420").append("password","qwerty");
         table.insertOne(document);
+
+        Document document2 = new Document("name","Eugene").append("phone","2312").append("password","none");
+        table.insertOne(document2);
+
+        for (Document current: table.find())
+        {
+            System.out.println(current.toJson());
+        }
 
         for (Document cur:table.find())
         {
